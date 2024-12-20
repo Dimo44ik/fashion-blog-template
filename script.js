fcf8100b63f0715df1252123b0dd3808f7b8823b
Dimo44ik <dimakrutoj694@gmail.com>
2024-12-20 18:30:59 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__list');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('open'); // Анимация бургера
-    menu.classList.toggle('open');   // Показываем/скрываем меню
-});
